d438b46b84a7a357b9f910c00c65528f338d1bf9
tianxiexingyun <tianxiexingyun@qq.com>
2022-08-31 16:09:19 +0800

    完成redisson对接配置

--- src/main/java/org/chobit/spring/redlock/RedLockConfiguration.java
+++ src/main/java/org/chobit/spring/redlock/RedLockConfiguration.java
@@ -5,6 +5,10 @@ import org.chobit.spring.redlock.interceptor.BeanFactoryRedLockAttributeSourceAd
 import org.chobit.spring.redlock.interceptor.RedLockAttributeSource;
 import org.chobit.spring.redlock.interceptor.RedLockInterceptor;
 import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.ClusterServersConfig;
+import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
@@ -13,22 +17,70 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Role;
 
+import java.util.List;
+
+
+import static jodd.util.StringUtil.isNotBlank;
+
 /**
  * @author rui.zhang
  */
 @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 @Configuration
-@ConditionalOnClass({Redisson.class})
+@ConditionalOnClass({Redisson.class, RedissonClient.class})
 @EnableConfigurationProperties(RedisProperties.class)
 public class RedLockConfiguration {
 
 
-    private final RedisProperties properties;
+    @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public RedissonClient redissonClient(RedisProperties properties) {
+        int timeout = 10000;
+        if (null != properties.getTimeout()) {
+            timeout = (int) properties.getTimeout().toMillis();
+        }
 
+        Config cfg = new Config();
 
-    public RedLockConfiguration(RedisProperties redisProperties) {
-        this.properties = redisProperties;
+        if (null != properties.getCluster()) {
+            List<String> nodes = properties.getCluster().getNodes();
+            ClusterServersConfig csc = cfg.useClusterServers()
+                    .addNodeAddress(convert(nodes))
+                    .setConnectTimeout(timeout);
+            if (isNotBlank(properties.getPassword())) {
+                csc.setPassword(properties.getPassword());
+            }
+        } else {
+            String protocol = "redis://";
+            if (properties.isSsl()) {
+                protocol = "rediss://";
+            }
+            SingleServerConfig ssc = cfg.useSingleServer()
+                    .setAddress(protocol + properties.getHost() + ":" + properties.getPort())
+                    .setConnectTimeout(timeout)
+                    .setDatabase(properties.getDatabase());
+            if (isNotBlank(properties.getPassword())) {
+                ssc.setPassword(properties.getPassword());
+            }
+        }
+
+        return Redisson.create(cfg);
     }
+
+
+    private String[] convert(List<String> nodes) {
+        String[] result = new String[nodes.size()];
+        for (int i = 0; i < nodes.size(); i++) {
+            String node = nodes.get(i);
+            if (node.startsWith("redis://") || node.startsWith("rediss://")) {
+                result[i] = node;
+            } else {
+                result[i] = "redis://" + node;
+            }
+        }
+        return result;
+    }
+
 
     @Bean
     @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
